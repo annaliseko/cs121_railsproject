70849840833d899b6f561ffb138b3d4e4d7d3a9e
pmcdonough35 <pmcdonough35@gmail.com>
2017-09-18 19:11:22 -0700

    Added username and settings path

--- fedup/db/schema.rb
+++ fedup/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918072300) do
+ActiveRecord::Schema.define(version: 20170919014901) do
 
   create_table "directions", force: :cascade do |t|
     t.text "step"
@@ -54,8 +54,10 @@ ActiveRecord::Schema.define(version: 20170918072300) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
